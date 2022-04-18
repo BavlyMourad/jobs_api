1181781b93bf51eee57a4daf0c454168e67847fb
BavlyMourad <bavleymourad@gmail.com>
2022-04-18 11:24:09 +0200

    Added update job & delete job features

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,6 @@
 // 4th File
 
+const { findById, findByIdAndDelete } = require("../models/jobs")
 const Job = require("../models/jobs")
 const geoCoder = require("../utils/geocoder")
 
@@ -22,6 +23,47 @@ exports.newJob = async (req, res, next) => {
     success: true,
     message: "Job created",
     data: job,
+  })
+}
+
+// Update a job => /api/v1/job/:id
+exports.updateJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id)
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    })
+  }
+
+  job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    success: true,
+    message: "Job updated",
+  })
+}
+
+// Delete a job => /api/v1/job/:id
+exports.deleteJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id)
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    })
+  }
+
+  job = await Job.findByIdAndDelete(req.params.id) // ALso there is Job.remove()
+
+  res.status(200).json({
+    success: true,
+    message: "Job deleted",
   })
 }
 
--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,8 @@ const {
   getJobs,
   newJob,
   getJobsInRadius,
+  updateJob,
+  deleteJob,
 } = require("../controllers/jobsController")
 
 // Route to all jobs
@@ -14,6 +16,9 @@ router.route("/jobs").get(getJobs)
 
 // Route to new job
 router.route("/job/new").post(newJob)
+
+// Route to update & delete job, since they use same route we add delete on put
+router.route("/job/:id").put(updateJob).delete(deleteJob)
 
 // Route to jobs around me in radius/distance
 router.route("/jobs/:zipcode/:distance").get(getJobsInRadius)
